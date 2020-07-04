56f74b9ad00baaefa975511e252bab66e1e49516
Daniel <lungu.daniel94@gmail.com>
2020-07-05 02:00:21 +0300

    Part-2 -> add step defs - with implementation

--- src/test/java/org/travelers/posts/cucumber/stepdefs/BackgroundStepDefs.java
+++ src/test/java/org/travelers/posts/cucumber/stepdefs/BackgroundStepDefs.java
@@ -1,19 +1,32 @@
 package org.travelers.posts.cucumber.stepdefs;
 
 import io.cucumber.datatable.DataTable;
+import io.cucumber.datatable.DataTableType;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.travelers.posts.domain.Post;
+import org.travelers.posts.repository.PostRepository;
+
+import java.util.List;
 
 public class BackgroundStepDefs extends StepDefs {
 
+    private List<Post> posts;
+
+    @Autowired
+    private PostRepository postRepository;
+
     @Given("post with the following attributes")
     public void post_with_following_attributes(DataTable dataTable) {
-
+        DataTableType.entry(Post.class);
+        this.posts = dataTable.asList(Post.class);
     }
 
     @When("post already exists")
     public void post_already_exists() {
-
+        postRepository.deleteAll();
+        postRepository.saveAll(posts);
     }
 
 }
--- src/test/java/org/travelers/posts/cucumber/stepdefs/DeletePostStepDefs.java
+++ src/test/java/org/travelers/posts/cucumber/stepdefs/DeletePostStepDefs.java
@@ -1,17 +1,57 @@
 package org.travelers.posts.cucumber.stepdefs;
 
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.travelers.posts.web.rest.PostResource;
+
+import static org.aspectj.bridge.MessageUtil.fail;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 
 public class DeletePostStepDefs extends StepDefs {
 
-    @When("login user {string} wants to delete post with the id {int}")
-    public void user_wants_delete_post_with_id(String login, int id) {
+    @Autowired
+    private PostResource postResource;
 
+    private MockMvc restUserMockMvc;
+
+    @Before
+    public void setup() {
+        this.restUserMockMvc = MockMvcBuilders.standaloneSetup(postResource).build();
+    }
+
+    @When("login user {string} wants to delete post with the id {int}")
+    public void user_wants_delete_post_with_id(String login, int id) throws Exception {
+        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+        securityContext.setAuthentication(new UsernamePasswordAuthenticationToken(login, login));
+        SecurityContextHolder.setContext(securityContext);
+
+        actions = restUserMockMvc.perform(delete("/api/post/" + id)
+            .accept(MediaType.APPLICATION_JSON));
     }
 
     @Then("the delete is {string}")
     public void delete_is(String expectedResult) {
+        int statusCode = actions.andReturn().getResponse().getStatus();
+
+        switch (expectedResult) {
+            case "SUCCESSFUL":
+                assertThat(statusCode).isIn(200, 201);
+                break;
+            case "FAIL":
+                assertThat(statusCode).isBetween(400, 504);
+                break;
+            default:
+                fail("Unexpected error");
+        }
 
     }
 
