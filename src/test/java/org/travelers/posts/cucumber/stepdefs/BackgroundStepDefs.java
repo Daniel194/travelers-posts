2eb2b6be2cd96e13938390f0c6be3b6f42192388
Daniel <lungu.daniel94@gmail.com>
2020-07-04 21:53:14 +0300

    Part-2 -> add step defs - without content

---
+++ src/test/java/org/travelers/posts/cucumber/stepdefs/BackgroundStepDefs.java
@@ -0,0 +1,19 @@
+package org.travelers.posts.cucumber.stepdefs;
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class BackgroundStepDefs extends StepDefs {
+
+    @Given("post with the following attributes")
+    public void post_with_following_attributes(DataTable dataTable) {
+
+    }
+
+    @When("post already exists")
+    public void post_already_exists() {
+
+    }
+
+}
